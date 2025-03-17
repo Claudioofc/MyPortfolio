f79745d41710fea32cf30ada0c75027183a69704
tiago.santos <tiago.santos@ammovarejo.com.br>
2025-03-17 16:44:26 -0300

    o navbar foi desativado

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Footer from "./components/StyledButton/Footer/footer"
-import NavBar from "./components/StyledButton/NavBar/NavBar"
+// import NavBar from "./components/StyledButton/NavBar/NavBar"
 import AboutSection from "./pages/Home/sections/AboutSection/AboutSection"
 import HeroSection from "./pages/Home/sections/HeroSection/HeroSection"
 import ProjectsSection from "./pages/Home/sections/ProjectsSection/ProjectsSection"
@@ -9,7 +9,7 @@ const App: React.FC = () => {
 
   return (
     <>
-      <NavBar />
+      {/* <NavBar /> */}
       <HeroSection />
       <AboutSection />
       <ProjectsSection />
