49bfc59eda45b705506773ccb050aef0d8995dd7
tiagostz <tiagosantos.01016@gmail.com>
2025-03-18 12:13:57 -0300

    move main para raiz

--- main.tsx
+++ main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './src/App.tsx'
 import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
-import theme from './theme';
+import theme from './src/theme.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
